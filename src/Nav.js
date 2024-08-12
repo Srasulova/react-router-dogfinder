80a95e4ec926ed0b2e89d0f830e0aaac4a9cc8f4
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-08-12 16:51:30 -0400

    add some styling to Nav component

--- src/Nav.js
+++ src/Nav.js
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom";
 function Nav({ dogs }) {
   return (
     <nav>
-      <ul>
+      <ul style={{ display: "flex", listStyle: "none" }}>
         {dogs.map((dog) => (
-          <li key={dog.name}>
+          <li style={{ marginLeft: "7px" }} key={dog.name}>
             <NavLink to={`/dogs/${dog.name.toLowerCase()}`}>{dog.name}</NavLink>
           </li>
         ))}
